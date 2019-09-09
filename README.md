d89eb2c15d98aabcab5edf028b005bbbb7e99d52
Aleksandar B <aspireledd@gmail.com>
2019-09-09 20:53:50 +0200

    Updates

--- README.md
+++
@@ -1,1 +0,0 @@
-# aleksandar-b.github.io
--- postcss.config.js
+++
@@ -1,7 +0,0 @@
-const tailwindcss = require("tailwindcss")
-module.exports = {
-  plugins: [
-    tailwindcss("./tailwindcss/tailwind.config.js"),
-    require("autoprefixer"),
-  ],
-}
