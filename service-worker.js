56af105ca19ead10a79fb45ee8c8c42e387d6fd9
Aleksandar B <aspireledd@gmail.com>
2019-09-10 13:48:01 +0200

    Updates

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.001ab268735adfa70e01a3b5e5e33f9b.js"
+  "/precache-manifest.c1406c0bcadc4e8263c9365b1cb22ad5.js"
 );
 
 workbox.clientsClaim();
