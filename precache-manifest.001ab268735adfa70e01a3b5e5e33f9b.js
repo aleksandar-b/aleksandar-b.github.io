4beb62e5e78aeadd9c7c4b9247d799cd5f77a954
Aleksandar B <aspireledd@gmail.com>
2019-09-10 13:52:55 +0200

    Updates

--- precache-manifest.001ab268735adfa70e01a3b5e5e33f9b.js
+++ precache-manifest.001ab268735adfa70e01a3b5e5e33f9b.js
@@ -4,19 +4,19 @@ self.__precacheManifest = [
     "url": "/static/js/runtime~main.42ac5946.js"
   },
   {
-    "revision": "f2a0d29c627f5ec5ca78",
-    "url": "/static/js/main.f2a0d29c.chunk.js"
+    "revision": "0a1b92dbc70fe519f9df",
+    "url": "/static/js/main.0a1b92db.chunk.js"
   },
   {
     "revision": "0d93b29b27d1d199691f",
     "url": "/static/js/2.0d93b29b.chunk.js"
   },
   {
-    "revision": "f2a0d29c627f5ec5ca78",
+    "revision": "0a1b92dbc70fe519f9df",
     "url": "/static/css/main.0b9d90aa.chunk.css"
   },
   {
-    "revision": "43d5dd75074483269674366d8452c7cd",
+    "revision": "c8037cd1b69cf2909a6a69cb7301af32",
     "url": "/index.html"
   }
 ];
